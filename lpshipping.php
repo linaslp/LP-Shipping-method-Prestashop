dec5f0861b74c94d3d6994c50610f420012be02e
margud <marius.gudauskis@invertus.eu>
2021-12-23 15:40:57 +0200

    fixed dispaly when no country is selected

--- lpshipping.php
+++ lpshipping.php
@@ -978,7 +978,19 @@ class LPShipping extends CarrierModule
 
         $deliveryAddress = new Address($cart->id_address_delivery);
         $country = new Country($deliveryAddress->id_country);
-        if ($country->iso_code === 'LT' || $country->iso_code == null) {
+        if ($country->iso_code == null) {
+            if (
+                (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_HOME') ||
+                (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_ABROAD') ||
+                (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_TERMINAL') ||
+                (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_POST') ||
+                (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_CARRIER_ABROAD')
+            ) {
+                return $shippingCost;
+            }
+        }
+
+        if ($country->iso_code === 'LT') {
             if (
                 (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_HOME') ||
                 (int)$carrier->id === (int)Configuration::get('LP_SHIPPING_EXPRESS_CARRIER_ABROAD') ||

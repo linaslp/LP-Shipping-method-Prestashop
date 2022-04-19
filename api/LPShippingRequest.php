ff176f1003efe7a1a67c6cc7f167bb5c833104fb
margud <marius.gudauskis@invertus.eu>
2022-04-19 17:30:31 +0300

    fixed timeout issue with auth API call

--- api/LPShippingRequest.php
+++ api/LPShippingRequest.php
@@ -159,7 +159,7 @@ class LPShippingRequest extends LPShippingBaseRequest
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 10,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
